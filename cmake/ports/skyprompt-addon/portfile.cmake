7415486015b6317319544076e372538701d3f5a2
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2025-08-20 02:28:24 +0200

    Update portfile.cmake

--- cmake/ports/skyprompt-addon/portfile.cmake
+++ cmake/ports/skyprompt-addon/portfile.cmake
@@ -2,9 +2,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO QTR-Modding/SkyPromptAddOn
-    REF 0
-    SHA512 0
-    HEAD_REF main
+    REF c985e1aa6cd2820b089dac512a908644ca5758cc
+    SHA512 bc6e8cad360fef94028e079063d1437a5314659c100118a3062c5629340191210b0abf8f3a5219e314e62181721d25d15bfe6bd0ba22620ba2b2fb8c473666ba
+    HEAD_REF update
 )
 
 # Install codes
